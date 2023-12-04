8530d43b6b51d2abede049c5facbdc52afed70dd
Gk-dev-sebastian <sebastian.garcia0583@gmail.com>
2023-12-03 23:12:50 -0500

    schedule

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -3,25 +3,25 @@ const router = express.Router();
 
 const scheduleController = require('../controllers/schedule.controller');
 
-const { validateAdminRole } = require('../middleware/validateRole'); 
+//const { validateAdminRole } = require('../middleware/validateRole'); 
 
 // Obtener el horario de una semana específica
 router.get('/week', scheduleController.getWeekSchedule);
 
 // Añadir una nueva clase a un día específico del horario
-router.post('/add-class', validateAdminRole, scheduleController.addClassToSchedule);
+router.post('/add-class', scheduleController.addClassToSchedule);
 
 //
 router.post('/create-week', scheduleController.createScheduleForWeek);
 
 // Crear un nuevo día en el horario
-router.post('/create-day', validateAdminRole, scheduleController.createDayInSchedule);
+router.post('/create-day', scheduleController.createDayInSchedule);
 
 // Actualizar un día específico en el horario
-router.put('/update-day', validateAdminRole, scheduleController.updateDayInSchedule);
+router.put('/update-day', scheduleController.updateDayInSchedule);
 
 // Eliminar un día específico del horario
-router.delete('/delete-day', validateAdminRole, scheduleController.deleteDayInSchedule);
+router.delete('/delete-day', scheduleController.deleteDayInSchedule);
 
 
 module.exports = router;
