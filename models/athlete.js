ea160107ffa77d2c1fcbc3c47bda5f6767493624
Gk-dev-sebastian <sebastian.garcia0583@gmail.com>
2024-05-27 09:12:47 -0400

    athlete

--- models/athlete.js
+++ models/athlete.js
@@ -6,24 +6,51 @@ const conditionsItemsSchema = Schema({
 });
 
 const athleteSchema = Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    _id:String,
+    
     email: String,
+    pass:String,
     name: String,
     lastName:String,
     dob:String,
+    address:String,
+    address2:String,
+    city:String,
+    state:String,
+    zip:String,
+    phone:String,
+    waiver:Boolean,
     nationality:String,
     weight:Number,
     age:Number,
-    rank:String,
     date_sign:String,
+    current_rank:String,
     last_promotion:String,
     next_promotion:String,
     healthConditions:Boolean,//*
-    conditions:[conditionsItemsSchema],
+        conditions:[conditionsItemsSchema],
+    signature:String,
+    type:String,
+    status:{
+        type:String,
+        required: true,
+        default: 'pending'
+    },
+    role:{
+        type:String,
+        required: true,
+        default: 'MEMBER'
+    },
+    active:Boolean,
+    dateSignedIn:String,
+    photoURL: String,
+    emailVerified: Boolean,
+
 });
+
+userSchema.method( 'toJSON', function() {
+const { __v, pass, ...object } = this.toObject();
+return object;
+}),
+
 
 module.exports = model('Athletes', athleteSchema);
