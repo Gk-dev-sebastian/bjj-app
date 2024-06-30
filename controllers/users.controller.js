eb2fd570e5f81fde3d26c640229a6e2b77a4caaa
Gk-dev-sebastian <sebastian.garcia0583@gmail.com>
2024-06-30 00:31:22 -0400

    users

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -21,8 +21,7 @@ const Athletes = require('../models/athlete');
     const getUserById = async(req, res) => {
         try {
             const userId = req.params.id;
-            const user = await User.findById(userId)
-                                   .populate('athletes');
+            const user = await User.findById(userId);
 
             if (!user) {
                 return res.status(404).json({ message: 'User not found' });
