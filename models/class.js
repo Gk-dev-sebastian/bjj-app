6674e873577746e8aba0e64b3ffdaec02ba256b4
Gk-dev-sebastian <sebastian.garcia0583@gmail.com>
2024-07-04 14:42:24 -0400

    class

--- models/class.js
+++ models/class.js
@@ -1,36 +1,10 @@
 const { Schema, model } = require('mongoose');
 
-const drillSchema = new Schema({
-    drill: String
-}, { _id: false });
-
-const curriculumSchema = new Schema({
-    groupName: String,
-    drills: [drillSchema]
-});
-
-const weekSchema = new Schema({
-    weekName: String, 
-    curriculum: [curriculumSchema]
-});
-
-const techniqueSchema = new Schema({
-    technique: String
-}, { _id: false });
-
-const techniquesGroupSchema = new Schema({
-    groupName: String,
-    techniques: [techniqueSchema]
-});
-
 const programSchema = new Schema({
     programName: String,
     description: String,
     date: String,
-    weeks: [weekSchema],
-    mainConcepts: [String],
-    mainPositions: [String],
-    techniques: [techniquesGroupSchema],
+    week: {},
     user: String
 });
 
@@ -54,10 +28,6 @@ const classSchema = new Schema({
         default: 'open',
         enum: ['open', 'closed']
     },
-    athletes: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Athlete' 
-    }]
 });
 
 // Middleware para cerrar automáticamente la clase si se alcanza la capacidad máxima
